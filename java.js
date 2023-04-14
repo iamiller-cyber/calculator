d7238a89181987869786d76a1a365f63dd40d2ac
Isaac <123217532+iamiller-cyber@users.noreply.github.com>
2023-04-14 09:45:15 -0400

    Add functionality to each button /n fix display

--- java.js
+++ java.js
@@ -1,16 +1,99 @@
-function add(x, y) {
-    return x + y
+
+
+function operate(x, operator, y) {
+    switch (operator) {
+        case "x":
+            isResult = true
+            return x * y;
+        case "+":
+            isResult = true
+            return x + y;
+        case "-":
+            isResult = true
+            return x - y;
+        case "/":
+            if (y === 0) {
+                return "can't do that"
+            }
+            isResult = true
+            return x / y;
+
+    }
+
 }
 
-function subtract(x, y) {
-    return x - y
-}
 
-function multiply(x, y) {
-    return x * y
-}
+const display = document.querySelector(".display")
+display.textContent = "0"
 
-function divide(x,y) {
-    return x / y
-}
+let output = display.textContent
+let firstNum = null
+let secondNum = null
+let operator = null
+let isResult = false
 
+
+const calc = document.querySelector(".calc")
+calc.addEventListener('click', function(e) {
+  if (!(e.target.dataset.action)) {
+    if (display.textContent === '0'|| isResult === true || operator !== null) {
+       display.textContent = e.target.textContent;
+       isResult = false;
+    }
+    else {
+        display.textContent += e.target.textContent;
+    }
+  }
+  if (e.target.dataset.action === "decimal") {
+    if (display.textContent.includes('.') || isResult === true) {
+        return;
+    }
+    else {
+        display.textContent += e.target.textContent;
+    }
+  }
+  if (e.target.dataset.action === "operator") {
+    if (operator === null) {
+        firstNum = parseFloat(display.textContent);
+        operator = e.target.textContent;
+    }
+    else {
+        secondNum = parseFloat(display.textContent);
+        result = operate(firstNum, operator, secondNum);
+        display.textContent = result;
+        result = firstNum;
+        operator = e.target.textContent;
+    }
+  }
+  if (e.target.dataset.action === "equals") {
+    if (firstNum === null) {
+        return
+    }
+    else {
+        secondNum = parseFloat(display.textContent);
+        result = operate(firstNum, operator, secondNum);
+        display.textContent = result;
+        operator = null;
+        firstNum = null;
+        secondNum = null;
+    }
+  }
+  if (e.target.dataset.action === "delete") {
+    if (display.textContent === '0') {
+        return
+    }
+    else if (display.textContent.length === 1) {
+        display.textContent = 0;
+    }
+    else {
+        display.textContent = display.textContent.slice(0, display.textContent.length -1)
+    }
+  }
+  if (e.target.dataset.action === "clear") {
+    firstNum = null
+    secondNum = null
+    operator = null
+    display.textContent = '0'
+    isResult = false
+  }
+})
